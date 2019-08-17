6675794e6fef2890694dba3853e772a9e2939d04
ozlelya <OZ-Lelya@meta.ua>
2019-08-17 14:51:51 +0300

    Create custom user Create model UserProfile

---
+++ dating_site/user_profile/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import UserProfile, MyUser
+
+# Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(MyUser)
+
---
+++ dating_site/user_profile/user_preferences.py
@@ -0,0 +1,67 @@
+GENDER_MALE = 0
+GENDER_FEMALE = 1
+CHOICES_GENDER = ((GENDER_MALE, r"Male"),
+                  (GENDER_FEMALE, r"Female"))
+
+AGE_CHOICES = ((1, "15-20"),
+               (2, "20-25"),
+               (3, "25-30"),
+               (4, "30-35"),
+               (5, '35-40'),
+               (6, "40-45"),
+               (7, "45-50"),
+               (8, "50-60"),
+               (9, "more than 60"))
+
+MUSIC_PREFERENCES = (("no preferences", "no preferences"),
+                     ("blues", "blues"),
+                     ("classical", "classical"),
+                     ("country", "country"),
+                     ("dance", "dance"),
+                     ("easy listening", "easy listening"),
+                     ("electronic", "electronic"),
+                     ("folk	", "folk"),
+                     ("heavy metal", "heavy metal"),
+                     ("hip hop", "hip hop"),
+                     ("jazz	", "jazz"),
+                     ("latin", "latin"),
+                     ("opera", "opera"),
+                     ("pop", "pop"),
+                     ("rap", "rap"),
+                     ("reggae", "reggae"),
+                     ("rock", "rock"),
+                     ("techno", "techno"),
+                     ("other", "other")
+)
+
+MOVIE_PREFERENCES = (("No preferences", "No preferences"),
+					 ("Adventure", "Adventure"),
+                     ("Comedy", "Comedy"),
+                     ("Drama", "Drama"),
+                     ("Action / thriller", "Action / thriller"),
+                     ("Animated film", "Animated film"),
+                     ("Horror", "Horror"),
+                     ("Documentary", "Documentary"),
+                     ("Science fiction", "Science fiction"),
+                     ("Romance comedy", "Romance comedy"),
+					 ("Other", "Other")
+)
+
+LITERATURE_PREFERENCES = (("No preferences", "No preferences"),
+						  ("Picture book", "Picture book"),
+						  ("Humor", "Humor"),
+						  ("Historical fiction", "Historical fiction"),
+						  ("Mystery", "Mystery"),
+						  ("Folklore", "Folklore"),
+						  ("Fan fiction", "Fan fiction"),
+						  ("Fairy tale", "Fairy tale"),
+						  ("Classic", "Classic"),
+						  ("Horror", "Horror"),
+						  ("Romance novel", "Romance novel"),
+						  ("Western", "Western"),
+						  ("Fantasy", "Fantasy"),
+						  ("Detective fiction", "Detective fiction"),
+						  ("Science fiction", "Science fiction"),
+						  ("Crime", "Crime"),
+						  ("Other", "Other")
+)
