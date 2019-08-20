4b149c7792245b63169d1c62b1fc0292956dfd95
ozlelya <OZ-Lelya@meta.ua>
2019-08-20 23:02:28 +0300

    Add simple Login/Logout system for user

--- dating_site/dating_site/urls.py
+++ dating_site/dating_site/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
 from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
@@ -26,3 +26,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    url('^accounts/', include('django.contrib.auth.urls')),
+]
